b84e34bf9a25ccf909df597d8fe0b3afc9990f6d
DallasJr <49690481+DallasJr@users.noreply.github.com>
2024-04-19 15:37:56 +0200

    index.html

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/src/", http.StripPrefix("/src/", http.FileServer(http.Dir("src"))))
+	http.HandleFunc("/", index)
+	//http.HandleFunc("/test", test)
+	fmt.Println("http://localhost/")
+	http.ListenAndServe("", nil)
+}
+
+type exportData struct {
+}
+
+var ExportData exportData
+
+func index(w http.ResponseWriter, r *http.Request) {
+	/*if strings.Contains(r.URL.Path, "favicon.ico") {
+		return
+	}*/
+	tmpl := template.Must(template.ParseFiles("src/templates/index.html"))
+	tmpl.Execute(w, ExportData)
+}
